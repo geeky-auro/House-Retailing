d4f7cb8cc2df1e5b0b81139d8f02ac71fb4af9c8
geeky-auro <aurosaswat.apk@gmail.com>
2023-08-23 00:39:39 +0530

    first commit

---
+++ venule/pages/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return HttpResponse('<h1>Hello</h1>');
+
+
+def about(request):
+    return render(request,'pages/index.html')
+
+def above(request):
+    return render(request,'pages/about.html')
