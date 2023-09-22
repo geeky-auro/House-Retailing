b57ced44a81a11e9241f3f0b75627ef953e288e8
geeky-auro <aurosaswat.apk@gmail.com>
2023-09-22 22:53:11 +0530

    Realtor Admin Page Customizations

--- venule/realtors/admin.py
+++ venule/realtors/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Realtors
 
+class ReaktorAdmin(admin.ModelAdmin):
+    list_display=('id','name','email','hire_date')
+    list_display_links=('id','name')
+    search_fields=('name',)
+    list_per_page=25
+
 admin.site.register(Realtors)
