8f41ad6e6ef42a15d32b124e23d0f4da47038a18
geeky-auro <aurosaswat.apk@gmail.com>
2023-09-22 22:39:37 +0530

    Adding Custom Functionality to Listing in Admin

--- venule/listings/admin.py
+++ venule/listings/admin.py
@@ -3,4 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Listing
 
-admin.site.register(Listing)
+
+
+class ListingAdmin(admin.ModelAdmin):
+    # Now we need to define what we can have in the table
+    list_display=('id','title','is_published','price','list_date','realtor')
+    list_display_links=('id','title')
+    # To have a filter box ;)
+    list_filter=('realtor',)
+    list_editable=('is_published',)
+    search_fields=('title','description','address','city','state','zipcode','price')
+    list_per_page=25
+
+admin.site.register(Listing,ListingAdmin)
