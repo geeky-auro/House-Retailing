15f35ca62d9f66fae6067aa84b4dd086dfafcf35
geeky-auro <aurosaswat.apk@gmail.com>
2023-09-22 16:49:09 +0530

    Updated Admin Area

--- venule/realtors/admin.py
+++ venule/realtors/admin.py
@@ -2,7 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Realtors
-from .models import Listings
 
 admin.site.register(Realtors)
-admin.site.register(Listings)
